ff94d778c12086c6f007b63e01c02dcbc7085a85
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2016-07-27 18:16:02 +0000

    adjusted indentiations on greeting.rb

--- code/greeting.rb
+++ code/greeting.rb
@@ -1,7 +1,7 @@
-    def greeting(greet, name)
-        name.each do |arg|
-           puts "#{greet}, #{arg}"
-        end
+def greeting(greet, name)
+    name.each do |arg|
+        puts "#{greet}, #{arg}"
     end
+end
     
-    greeting ARGV.shift, ARGV
+greeting ARGV.shift, ARGV
