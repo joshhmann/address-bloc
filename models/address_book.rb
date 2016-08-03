371b1cba8289e533cce7b98e80681b0215c760b2
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2016-08-03 04:12:59 +0000

    adding searching method

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,16 +8,45 @@ class AddressBook
         @entries = []
     end
         # #7
-    def import_from_csv(file_name)
-        csv_text = File.read(file_name)
-        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+  def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
         
         # #8
-        csv.each do |row|
-          row_hash = row.to_hash
-          add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+    
+    def binary_search(name)
+      
+      # #1
+      lower = 0
+      upper = entries.length - 1
+      
+      # #2
+      while lower <= upper
+        # #3
+        mid = (lower + upper) / 2
+        mid_name = entries[mid].name
+        
+        # #4
+        if name == mid_name
+          return entries[mid]
+          elsif name < mid_name
+            upper = mid - 1
+          elsif
+            lower = mid + 1
         end
+      end
+      
+      # #5
+      return nil
     end
+  end
+          
+  
+    
     def remove_entry(name, phone, email)
         delete_entry = nil
         
